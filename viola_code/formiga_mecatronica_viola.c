62021504f83aebb2a208f988287d4dc27a3bcaa0
FelipeFinco <felipefinco98@gmail.com>
2021-07-26 22:52:33 -0300

    Comecando a criar a maquina de estados

--- viola_code/formiga_mecatronica_viola.c
+++ viola_code/formiga_mecatronica_viola.c
@@ -1,9 +1,114 @@
 #include <stdio.h>
 #include <time.h>     
 #include <unistd.h>
- 
+#include <string.h>
+
+
+
+double map(double x, double in_min, double in_max, double out_min, double out_max) {
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
+//Different state of an ant robot
+enum states {
+    SENDING_COMMAND,
+    WAITING_MOVEMENT,
+	EXIT
+} state;
+
+
+//Different type events
+enum events {
+    COMMAND_SENT,
+    RESTART_LOOP,
+    STOP_LOOPING,
+};
+
+
 int main(int argc, char **argv)
 {
+	//start robot waiting to send command
+	state =  SENDING_COMMAND;
+	enum events event = RESTART_LOOP;	
+
+	//indicates that the robot will locomove forward
+	bool forward = true;					
+
+
+	switch(state) {
+		case SENDING_COMMAND:
+			char response[256];
+			printf("\nPor favor entrar um comando. (frente/tras/sair)\n");
+			scanf( "%s" , response);
+			if ((strcmp (response, "frente")) == 0)
+			{
+				event = COMMAND_SENT;
+				bool forward = true;
+				printf("\nAndando para frente!\n");
+			}
+			else if ((strcmp (response, "tras")) == 0)
+			{
+				event = COMMAND_SENT;
+				bool forward = false;
+				printf("\nAndando para tras!\n");
+			}
+			else if ((strcmp (response, "sair")) == 0)
+			{
+				event = STOP_LOOPING;
+			}
+			else 
+			{
+				printf("\nNao foi possivel compreender o comando!!!\n");
+				event = RESTART_LOOP;
+			}
+		
+			switch(event) {
+			case COMMAND_SENT:
+				state = WAITING_MOVEMENT;
+				break;
+			default:
+				exit(1);
+				break;
+			}       
+			break;
+			/*
+		case LOOP:
+			switch(event) {
+			case PRINT_HELLO:
+				printf("Hello World!\n");
+				break;
+			case STOP_LOOPING:
+				state = END;
+				break;
+			default:
+				exit(1);
+				break;
+			}
+			break;
+		case END:
+			exit(1);
+			break;
+		}
+		*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 	double timer = 0.0;
 	double timer_old = 0.0;
 	
@@ -23,6 +128,7 @@ int main(int argc, char **argv)
 		}
 		end = clock();
 	}
+	*/
 	
 	printf("\nHello World !!!\n");        // print "Hello World" message on screen
 	return(0);
@@ -30,6 +136,3 @@ int main(int argc, char **argv)
 
 
 
-double map(double x, double in_min, double in_max, double out_min, double out_max) {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
